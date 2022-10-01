4faee4272eed79d06cda54ce08a5b96516b8ff8c
zhangke <996347413zk@gmail.com>
2022-10-01 14:52:20 +0800

    计算两数的最小公倍数

---
+++ 其他类型/最小公倍数问题/计算两数的最小公倍数.py
@@ -0,0 +1,18 @@
+def CalLeastCommonMultiple(x, y):
+    """
+    给定两个正整数x和y，求两个数的最小公倍数
+    :param x: 输入正整数1
+    :param y: 输入正整数2
+    :return: 返回二者的最小公倍数
+    """
+    for t in range(1, y+1):
+        res = x * t
+        if res % y == 0:
+            return res
+
+
+# 测试
+if __name__ == "__main__":
+    a, b = 54, 24
+    result = CalLeastCommonMultiple(a, b)
+    print(f"通过计算: {a} 和 {b} 的最小公倍数是 {result} .")
