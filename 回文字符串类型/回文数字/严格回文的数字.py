2b4918912e4ce0c37e01e04306b6a0a8b8f16eb1
zhangke <996347413zk@gmail.com>
2022-10-02 17:23:16 +0800

    判断数字进制的字符串是否严格回文

---
+++ 回文字符串类型/回文数字/严格回文的数字.py
@@ -0,0 +1,8 @@
+def isStrictlyPalindromic(n):
+    """
+    给定正整数n，如果n在b进制下对应的字符串全都是回文的，则返回true，否则返回false；
+    b为2到n-2中的所有整数；
+    回文字符串是一种从前往后读和从后往前读都一样的字符串
+    :param n: 输入的正整数n [int]
+    :return: 是否构成回文 [bool]
+    """
