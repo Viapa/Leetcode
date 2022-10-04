c20b4b5274ac40d0f7f69ad176043840a54c6f6f
zhangke <996347413zk@gmail.com>
2022-10-04 12:58:44 +0800

    网址的简单加密与解密

--- 其他类型/加密与解密/TinyURL的加密与解密.py
+++ 其他类型/加密与解密/TinyURL的加密与解密.py
@@ -5,28 +5,35 @@ def TinyURL(url):
     :param url: 输入的网址 [str]
     :return: 返回加密网址 [str] 和 解密网址 [str]
     """
+    # 思路是基于ascii码值进行处理后还原
     def encode(longUrl):
-        tmp_url = longUrl.replace("https", "http")
-        encodeUrl = tmp_url.replace(".com/", "?")
+        asc_ls = []
+        for c in longUrl:
+            ascii_c = str(ord(c))
+            asc_ls.append(ascii_c)
+        encodeUrl = ",".join(asc_ls)
         return encodeUrl
 
     def decode(shortUrl):
-        tmp_url = shortUrl.replace("http", "https")
-        decodeUrl = tmp_url.replace("?", ".com/")
+        res_ls = []
+        asc_ls = shortUrl.split(",")
+        for num in asc_ls:
+            c = chr(int(num))
+            res_ls.append(c)
+        decodeUrl = "".join(res_ls)
         return decodeUrl
 
     encode_url = encode(url)
     decode_url = decode(encode_url)
 
-    assert len(encode_url) < len(url), "加密失败, 加密网址必须短于原网址长度!"
     assert decode_url == url, "解密失败, 解密网址必须与原网址相同!"
     return encode_url, decode_url
 
 
 # 测试
 if __name__ == "__main__":
-    url = "https://leetcode.com/problems/design-tinyurl"
+    url = "http://www.leetcode.com/faq/?id=10"
     encode_url, decode_url = TinyURL(url)
-    print(f"加密成功!, 输入原网址为：{url}")
-    print(f"经过加密的网址为：{encode_url}")
-    print(f"经过解密的网址为：{decode_url}")
+    print(f"加密成功!, 输入原网址为：'{url}'")
+    print(f"经过加密的网址为：'{encode_url}'")
+    print(f"经过解密的网址为：'{decode_url}'")
