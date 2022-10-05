6bbdb1c4fc4377120eb5b72226bf0151a68b4717
zhangke <996347413zk@gmail.com>
2022-10-05 17:40:12 +0800

    收集垃圾的最少时间

--- 线性规划问题/2391-收集垃圾的最少时间.py
+++ 线性规划问题/2391-收集垃圾的最少时间.py
@@ -1,9 +1,14 @@
 def garbageCollection(garbage, travel):
     """
-
-    :param garbage:
-    :param travel:
-    :return:
+    有三种垃圾车需要去不同房屋回收垃圾，garbage表示不同位置的房屋，garbage[i]表示房间里的垃圾
+    垃圾分为三种，'M', 'P', 'G' 分别表示金属、纸张和玻璃
+    三辆车均从起点开始，分别按次序（M->G）向房屋前进
+    每回收任意一个垃圾需要1分钟，没有对应垃圾的屋子时，相对的车辆也可以不用前进
+    travel表示不同屋子之间的距离，travel[0]表示从0到1屋子需要的时间
+    请求出收拾完所有垃圾需要花费的最少总分钟数
+    :param garbage: 不同房间的垃圾集合 [list[str]]
+    :param travel:  不同屋子之间的距离 [list[int]]
+    :return: 收拾完所有垃圾最少需要花费的时间（分钟） [int]
     """
     timeCost = 0
     metalNum, paperNum, glassNum = 0, 0, 0
