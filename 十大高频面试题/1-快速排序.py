375dbe1c6d18e48b81f9b7dca97e9fb801fdd3eb
zhangke43 <zhangke43@meituan.com>
2023-03-06 16:20:10 +0800

    基于递归和双指针的快速排序

--- 十大高频面试题/1-快速排序.py
+++ 十大高频面试题/1-快速排序.py
@@ -6,20 +6,20 @@ def quick_sort(arr, reversed=False):
             pivot = arr[left]
             i = left
             j = right
-            while i < j:
-                while i < j and arr[j] > pivot:
+            while i < j:  # 大循环条件判断
+                while i < j and arr[j] > pivot:  # 从右往左寻找比pivot小的数
                     j -= 1
-                arr[i] = arr[j]
-                while i < j and arr[i] <= pivot:
+                arr[i] = arr[j]   # 覆盖i元素的值
+                while i < j and arr[i] <= pivot:  # 从左往右寻找pivot大的数
                     i += 1
-                arr[j] = arr[i]
-            arr[i] = pivot
-            recursive_sort(arr, left, i - 1)
-            recursive_sort(arr, i + 1, right)
-
+                arr[j] = arr[i]  # 覆盖j元素的值
+            arr[i] = pivot  # 将pivot放到中间位置，即此时i元素位置
+            recursive_sort(arr, left, i - 1)  # 对pivot左侧元素的子数组进行递归排序
+            recursive_sort(arr, i + 1, right)  # 对pivot右侧元素的子数组进行递归排序
+    # 初始化执行快速排序
     n = len(arr)
     recursive_sort(arr, 0, n - 1)
-    if reversed:
+    if reversed:  # 是否逆序写出
         return arr[::-1]
     else:
         return arr
